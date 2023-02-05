1d90267f9f84a47fba6682f218038f2a947462fd
Funt <funt_88@mail.ru>
2023-02-06 02:42:25 +0300

    fixes

--- Seminar 2 (for, while).py
+++ Seminar 2 (for, while).py
@@ -1,48 +1,48 @@
-# for i in range(5):
-#     print(i, end=' ')
+for i in range(5):
+    print(i, end=' ')
 
-# i = 0
-# while i < 5:
-#     i += 1
-#     if i == 5:
-#         print(i)
-#     else:
-#         print(i, end=', ')
+i = 0
+while i < 5:
+    i += 1
+    if i == 5:
+        print(i)
+    else:
+        print(i, end=', ')
 
-# n = int(input('Введите число: '))
-# result = 1
-# i = 2
-# while i <= n:
-#     result *= i
-#     i += 1
-# print(f"Факториал числа {n} -> {result}")
+n = int(input('Введите число: '))
+result = 1
+i = 2
+while i <= n:
+    result *= i
+    i += 1
+print(f"Факториал числа {n} -> {result}")
 
-# n = int(input('Введите число: '))
-# a0 = 0
-# a1 = 1
-# count = 1
-# while a0 < n:
-#     x = a0 + a1
-#     a0 = a1
-#     a1 = x
-#     count += 1
-# if a0 != n:
-#     print(f'Число {n} не является числом Фибоначчи')
-# else:
-#     print(count)
+n = int(input('Введите число: '))
+a0 = 0
+a1 = 1
+count = 1
+while a0 < n:
+    x = a0 + a1
+    a0 = a1
+    a1 = x
+    count += 1
+if a0 != n:
+    print(f'Число {n} не является числом Фибоначчи')
+else:
+    print(count)
 
-# n = int(input('Введите кол-во дней: '))
-# count = 0
-# max_count = 0
-# for i in range(n):
-#     temp = int(input())
-#     if temp > 0:
-#         count += 1
-#     else:
-#         if max_count < count:
-#             max_count = count
-#         count = 0
-# print(max_count)
+n = int(input('Введите кол-во дней: '))
+count = 0
+max_count = 0
+for i in range(n):
+    temp = int(input())
+    if temp > 0:
+        count += 1
+    else:
+        if max_count < count:
+            max_count = count
+        count = 0
+print(max_count)
 
 n = int(input('Введите кол-во арбузов: '))
 max = 0
--- Seminar 3 (list, dictionary + join).py
+++ Seminar 3 (list, dictionary + join).py
@@ -1,52 +1,52 @@
-# # Вывести кол-во уникальных элементов.
-# list_1 = [1, 3, 4, 5, 5, 5, 6, 7]
-# result_list = list()
-# for i in list_1:
-#     if i not in result_list:
-#         result_list.append(i)
-# print(result_list)
-# print(len(result_list))
-# # или
-# list_1 = [1, 3, 4, 5, 5, 5, 6, 7]
-# result_list = list()
-# u = list_1.union(result_list)
-# print(u)
-# print(len(u))
-# # или
-# list_1 = [1, 3, 4, 5, 5, 5, 6, 7]
-# result_list = set(list_1)
-# print(result_list)
-# print(len(result_list))
+# Вывести кол-во уникальных элементов.
+list_1 = [1, 3, 4, 5, 5, 5, 6, 7]
+result_list = list()
+for i in list_1:
+    if i not in result_list:
+        result_list.append(i)
+print(result_list)
+print(len(result_list))
+# или
+list_1 = [1, 3, 4, 5, 5, 5, 6, 7]
+result_list = list()
+u = list_1.union(result_list)
+print(u)
+print(len(u))
+# или
+list_1 = [1, 3, 4, 5, 5, 5, 6, 7]
+result_list = set(list_1)
+print(result_list)
+print(len(result_list))
 
-# # Необходимо сдвинуть всю последовательность (сдвиг - циклический) на K элементов вправо.
-# list_1 = [1, 2, 3, 4, 5]
-# k = int(input())
-# k = k % len(list_1)
-# list_result = list()
-# for i in range(k):
-#     list_result.append(list_1[len(list_1) - k + i])
-# for i in range(len(list_1) - k):
-#     list_result.append(list_1[i])
-# print(list_result)
-# # или
-# list_1 = [1, 2, 3, 4, 5]
-# k = int(input())
-# k = k % len(list_1)
-# for i in range(k):
-#     list_1.insert(0, list_1.pop(-1))
-# print(list_1)
+# Необходимо сдвинуть всю последовательность (сдвиг - циклический) на K элементов вправо.
+list_1 = [1, 2, 3, 4, 5]
+k = int(input())
+k = k % len(list_1)
+list_result = list()
+for i in range(k):
+    list_result.append(list_1[len(list_1) - k + i])
+for i in range(len(list_1) - k):
+    list_result.append(list_1[i])
+print(list_result)
+# или
+list_1 = [1, 2, 3, 4, 5]
+k = int(input())
+k = k % len(list_1)
+for i in range(k):
+    list_1.insert(0, list_1.pop(-1))
+print(list_1)
 
-# # Напишите программу для печати всех уникальных значений в словарях.
-# list_1 = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {
-#     "VII": "S005"}, {"V ": "S009"}, {"VIII": "S007"}]
-# set_1 = set()
-# for i in list_1:
-#     for j in i:
-#         set_1.add(i[j])
-# print(set_1)
-# # или способы, как в 1ой задаче
+# Напишите программу для печати всех уникальных значений в словарях.
+list_1 = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {
+    "VII": "S005"}, {"V ": "S009"}, {"VIII": "S007"}]
+set_1 = set()
+for i in list_1:
+    for j in i:
+        set_1.add(i[j])
+print(set_1)
+# или способы, как в 1ой задаче
 
-# Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером)
+# #Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером)
 n = [0, -1, 5, 2, 3]
 count = 0
 list_1 = list()
